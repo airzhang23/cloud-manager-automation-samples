891d446d0a739ce20794da0459b02e4c467849a0
erandarzi <eran.darzi@netapp.com>
2019-03-28 11:24:34 +0200

    terraform updates

--- terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "ids" {
-  description = "List of IDs of instances"
-  value       = "${aws_instance.occm_example.id}"
-}
-
-output "ip" {
-  description = "List of IPs of instances"
-  value       = "${aws_instance.occm_example.public_ip}"
-}
