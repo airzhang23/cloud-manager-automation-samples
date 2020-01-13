eea2462c6bb21ace6115a02f031d58604750bcd1
Dudu Bistrov <45168618+dudubis@users.noreply.github.com>
2020-01-13 16:11:51 +0200

    skip ssl verification at file download
    r = requests.get(terraformUrl, stream=True, verify = False)

--- scripts/get_latest_terraform.py
+++ scripts/get_latest_terraform.py
@@ -10,6 +10,6 @@ inner_ul = soup.find('ul')
 inner_items = [li.text.strip() for li in inner_ul.find_all('li')]
 ver = inner_items[1].split('_')[1]
 terraformUrl = 'https://releases.hashicorp.com/terraform/' + ver + '/terraform_' + ver + '_linux_amd64.zip'
-r = requests.get(terraformUrl, stream=True)
+r = requests.get(terraformUrl, stream=True, verify = False)
 with open("terraform_" + ver + "_linux_amd64.zip", "wb") as code:
     code.write(r.content)
